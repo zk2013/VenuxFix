23ea4123aee5d9b77ccb4a1a4899570157bdb538
plutochn <378925748@qq.com>
2017-09-11 18:20:40 +0800

    modify code

--- dexposedj/jni/dexposed.cpp
+++ dexposedj/jni/dexposed.cpp
@@ -2,7 +2,11 @@
 #include <jni.h>
 #include<android/log.h>
 #include <dlfcn.h>
+#include <sys/mman.h>
 #include "dexposed.h"
+
+// com.venustv.dexposedj
+#define DEXPOSED_CLASS "com/venustv/dexposedj/DexposedBridge"
 
 #define TAG "zj" // 这个是自定义的LOG的标识
 #define LOGD(...) __android_log_print(ANDROID_LOG_DEBUG,TAG ,__VA_ARGS__) // 定义LOGD类型
@@ -10,6 +14,26 @@
 #define LOGW(...) __android_log_print(ANDROID_LOG_WARN,TAG ,__VA_ARGS__) // 定义LOGW类型
 #define LOGE(...) __android_log_print(ANDROID_LOG_ERROR,TAG ,__VA_ARGS__) // 定义LOGE类型
 #define LOGF(...) __android_log_print(ANDROID_LOG_FATAL,TAG ,__VA_ARGS__) // 定义LOGF类型
+
+jclass dexposedClass = NULL;
+ClassObject* objectArrayClass = NULL;
+
+typedef void (*PTR_dvmCallMethod)(void* self, const Method* method, Object* obj,
+    JValue* pResult, ...);
+PTR_dvmCallMethod dvmCallMethod = NULL;
+
+typedef ArrayObject* (*PTR_dvmAllocArrayByClass)(ClassObject* arrayClass,
+    size_t length, int allocFlags);
+PTR_dvmAllocArrayByClass  dvmAllocArrayByClass = NULL;
+
+typedef ClassObject* (*PTR_dvmFindArrayClass)(const char* descriptor, Object* loader);
+PTR_dvmFindArrayClass dvmFindArrayClass= NULL;
+
+typedef Method* (*PTR_dvmSlotToMethod)(ClassObject* clazz, int slot);
+PTR_dvmSlotToMethod dvmSlotToMethod = NULL;
+
+typedef void (*PTR_dvmThrowIllegalArgumentException)(const char *);
+PTR_dvmThrowIllegalArgumentException  dvmThrowIllegalArgumentException = NULL;
 
  typedef void (*PTR_dvmLogExceptionStackTrace)(void) ;
 PTR_dvmLogExceptionStackTrace dvmLogExceptionStackTrace = NULL;
@@ -27,11 +51,40 @@ int RUNNING_PLATFORM_SDK_VERSION = 0;
 void* PTR_gDvmJit = NULL;
 size_t arrayContentsOffset = 0;
 
+Method* dexposedHandleHookedMethod = NULL;
+
 #ifndef PROPERTY_VALUE_MAX
 #define PROPERTY_VALUE_MAX  92
 #endif
 
 extern "C" int property_get(const char *key, char *value, const char *default_value);
+
+
+//_Z19dvmCheckClassAccessPK11ClassObjectS1_
+uintptr_t dvmCheckClassAccess = 0;
+
+//_Z19dvmCheckFieldAccessPK11ClassObjectPK5Field
+uintptr_t dvmCheckFieldAccess = 0;
+
+//_Z16dvmInSamePackagePK11ClassObjectS1_
+uintptr_t dvmInSamePackage  = 0;
+
+//_Z20dvmCheckMethodAccessPK11ClassObjectPK6Method
+uintptr_t dvmCheckMethodAccess = 0;
+
+void init_check_func(int dvm_handle) {
+ dvmInSamePackage = dlsym(dvm_handle, "_Z16dvmInSamePackagePK11ClassObjectS1_");
+   dvmCheckFieldAccess = dlsym(dvm_handle, "_Z19dvmCheckFieldAccessPK11ClassObjectPK5Field");
+    dvmCheckClassAccess = dlsym(dvm_handle, "_Z19dvmCheckClassAccessPK11ClassObjectS1_");
+
+    dvmCheckMethodAccess = dlsym(dvm_handle, "_Z20dvmCheckMethodAccessPK11ClassObjectPK6Method");
+
+    dvmThrowIllegalArgumentException = (PTR_dvmThrowIllegalArgumentException)dlsym(dvm_handle, "_Z32dvmThrowIllegalArgumentExceptionPKc");
+    dvmSlotToMethod = (PTR_dvmSlotToMethod)dlsym(dvm_handle, "_Z15dvmSlotToMethodP11ClassObjecti");
+    dvmAllocArrayByClass = (PTR_dvmAllocArrayByClass)dlsym(dvm_handle, "dvmAllocArrayByClass");
+    dvmFindArrayClass = (PTR_dvmFindArrayClass)dlsym(dvm_handle, "_Z17dvmFindArrayClassPKcP6Object");
+    dvmCallMethod = (PTR_dvmCallMethod)dlsym(dvm_handle, "_Z13dvmCallMethodP6ThreadPK6MethodP6ObjectP6JValuez");
+}
 
 void initTypePointers() {
     const char *error;
@@ -53,7 +106,8 @@ LOGI("sdk = %s",sdk );
                  LOGE("Could not find address for function dvmLogExceptionStackTrace: %s", error);
                 }
 
-int dvm_handle = (int)dlopen("libdvm.so",0);
+int dvm_handle = (int)dlopen("libdvm.so",RTLD_NOW);
+init_check_func(dvm_handle);
 
                dvmThreadSelf = (PTR_dvmThreadSelf)dlsym(dvm_handle, "_Z13dvmThreadSelfv");
              if ((error = dlerror()) != NULL || NULL == dvmThreadSelf ) {
@@ -146,6 +200,160 @@ LOGD("dwdwdwe");
     return ret;
 }
 
+#define PAGESIZE (4096)
+
+static void replaceAsm(uintptr_t function, unsigned const char* newCode, size_t len) {
+#ifdef __arm__
+    function = function & ~1;
+#endif
+    uintptr_t pageStart = function & ~(PAGESIZE-1);
+    size_t pageProtectSize = PAGESIZE;
+    if (function+len > pageStart+pageProtectSize)
+        pageProtectSize += PAGESIZE;
+
+    mprotect((void*)pageStart, pageProtectSize, PROT_READ | PROT_WRITE | PROT_EXEC);
+   //   LOGI("replaceAsm 1");
+    memcpy((void*)function, newCode, len);
+    mprotect((void*)pageStart, pageProtectSize, PROT_READ | PROT_EXEC);
+ //LOGI("replaceAsm 2");
+    __clear_cache((void*)function, (void*)(function+len));
+}
+
+static void patchReturnTrue(uintptr_t function) {
+// direct return true
+// why crash
+//return ;
+#ifdef __arm__
+
+ //MOVS	R0, #1
+ //BX	LR
+    unsigned const char asmReturnTrueThumb[] = { 0x01, 0x20, 0x70, 0x47 };
+
+   // mov r0,1
+    //  bx lr
+    unsigned const char asmReturnTrueArm[] = { 0x01, 0x00, 0xA0, 0xE3, 0x1E, 0xFF, 0x2F, 0xE1 };
+
+    LOGI("patchReturnTrue %08x", function);
+    if (function & 1)
+    {
+    LOGI("patchReturnTrue arm thumb");
+     replaceAsm(function, asmReturnTrueThumb, sizeof(asmReturnTrueThumb));
+    }
+    else
+    {
+        replaceAsm(function, asmReturnTrueArm, sizeof(asmReturnTrueArm));
+        LOGI("patchReturnTrue arm arm");
+    }
+
+#else
+  //  unsigned const char asmReturnTrueX86[] = { 0x31, 0xC0, 0x40, 0xC3 };
+   // replaceAsm(function, asmReturnTrueX86, sizeof(asmReturnTrueX86));
+#endif
+}
+
+__inline__ bool dvmIsStaticMethod(const Method* method) {
+    return (method->accessFlags & ACC_STATIC) != 0;
+}
+
+static void dexposedCallHandler(const u4* args, void* pResult, const Method* method, void* self);
+
+static inline bool dexposedIsHooked(const Method* method) {
+    return (method->nativeFunc == &dexposedCallHandler);
+}
+
+static void dexposedCallHandler(const u4* args, void* pResult, const Method* method, void* self) {
+
+    LOGI("dexposedCallHandler called");
+    if (!dexposedIsHooked(method)) {
+        dvmThrowIllegalArgumentException("could not find Dexposed original method - how did you even get here?");
+        return;
+    }
+
+    DexposedHookInfo* hookInfo = (DexposedHookInfo*) method->insns;
+    Method* original = (Method*) hookInfo;
+    Object* originalReflected = hookInfo->reflectedMethod;
+    Object* additionalInfo = hookInfo->additionalInfo;
+
+    // convert/box arguments
+    const char* desc = &method->shorty[1]; // [0] is the return type.
+    Object* thisObject = NULL;
+    size_t srcIndex = 0;
+    size_t dstIndex = 0;
+
+    // for non-static methods determine the "this" pointer
+    if (!dvmIsStaticMethod(original)) {
+        thisObject = (Object*) args[0];
+        srcIndex++;
+    }
+
+    ArrayObject* argsArray = dvmAllocArrayByClass(objectArrayClass, strlen(method->shorty) - 1, ALLOC_DEFAULT);
+    if (argsArray == NULL) {
+        return;
+    }
+     // call the Java handler function
+    JValue result;
+    dvmCallMethod(self, dexposedHandleHookedMethod, NULL, &result,
+        originalReflected, (int) original, additionalInfo, thisObject, argsArray);
+}
+
+static void com_taobao_android_dexposed_DexposedBridge_hookMethodNative(JNIEnv* env, jclass clazz, jobject reflectedMethodIndirect,
+            jobject declaredClassIndirect, jint slot, jobject additionalInfoIndirect) {
+        LOGI("com_taobao_android_dexposed_DexposedBridge_hookMethodNative called");
+
+         if (declaredClassIndirect == NULL || reflectedMethodIndirect == NULL) {
+               dvmThrowIllegalArgumentException("method and declaredClass must not be null");
+                return;
+            }
+        ClassObject* declaredClass = (ClassObject*) dvmDecodeIndirectRef(dvmThreadSelf(), declaredClassIndirect);
+        if (declaredClass->descriptor != NULL ) {
+        // Lcom/venustv/venuxfix/test/Cat;
+            LOGI("declaredClass->descriptor %s",declaredClass->descriptor);
+        }
+
+        Method* method = dvmSlotToMethod(declaredClass, slot);
+        if (method == NULL) {
+         //   dvmThrowNoSuchMethodError("could not get internal representation for method");
+         dvmThrowIllegalArgumentException("could not get internal representation for method");
+            return;
+        }
+        LOGI("declaredClass->method %s",method->name);
+        if (dexposedIsHooked(method)) {
+            // already hooked
+            LOGI("declaredClass->method %s already hooked",method->name);
+            return;
+        }
+        LOGI("declaredClass->method %s not hooked",method->name);
+
+         // Save a copy of the original method and other hook info
+        DexposedHookInfo* hookInfo = (DexposedHookInfo*) calloc(1, sizeof(DexposedHookInfo));
+        memcpy(hookInfo, method, sizeof(hookInfo->originalMethodStruct));
+        hookInfo->reflectedMethod = dvmDecodeIndirectRef(dvmThreadSelf(), env->NewGlobalRef(reflectedMethodIndirect));
+        hookInfo->additionalInfo = dvmDecodeIndirectRef(dvmThreadSelf(), env->NewGlobalRef(additionalInfoIndirect));
+         // Replace method with our own code
+        SET_METHOD_FLAG(method, ACC_NATIVE);
+        method->nativeFunc = &dexposedCallHandler;
+
+        method->insns = (const u2*) hookInfo;
+        method->registersSize = method->insSize;
+        method->outsSize = 0;
+
+        /*
+        if (PTR_gDvmJit != NULL) {
+              // reset JIT cache
+              MEMBER_VAL(PTR_gDvmJit, DvmJitGlobals, codeCacheFull) = true;
+          }//*/
+   }
+
+static const JNINativeMethod dexposedMethods[] = {
+    {"hookMethodNative", "(Ljava/lang/reflect/Member;Ljava/lang/Class;ILjava/lang/Object;)V", (void*)com_taobao_android_dexposed_DexposedBridge_hookMethodNative},
+};
+
+#define NELEM(x) (sizeof(x)/sizeof(x[0]))
+
+static int register_com_taobao_android_dexposed_DexposedBridge(JNIEnv* env) {
+    return env->RegisterNatives(dexposedClass, dexposedMethods, NELEM(dexposedMethods));
+}
+
 /**
  * called in JNI_OnLoad , className id NULL
  */
@@ -155,9 +363,76 @@ bool dexposedOnVmCreated(JNIEnv* env, const char* className) {
       keepLoadingDexposed = keepLoadingDexposed && dexposedInitMemberOffsets(env);
         if (!keepLoadingDexposed)
             return false;
+  LOGD("has run here");
 
-    return ret;
+ // disable some access checks
+ if (dvmCheckClassAccess)
+    patchReturnTrue((uintptr_t) dvmCheckClassAccess);
+else
+    LOGD("dvmCheckClassAccess = NULL");
+ LOGD("dvmCheckClassAccess = %08x",dvmCheckClassAccess);
+
+/*
+ if (dvmCheckFieldAccess)
+    patchReturnTrue((uintptr_t) &dvmCheckFieldAccess);
+else
+    LOGD("dvmCheckFieldAccess = NULL");
+
+   if (dvmInSamePackage)
+    patchReturnTrue((uintptr_t) &dvmInSamePackage);
+else
+    LOGD("dvmInSamePackage = NULL");
+
+  if (dvmCheckMethodAccess)
+    patchReturnTrue((uintptr_t) &dvmCheckMethodAccess);
+else
+    LOGD("dvmCheckMethodAccess = NULL");*/
+
+ env->ExceptionClear();
+
+    dexposedClass = env->FindClass(DEXPOSED_CLASS);
+dexposedClass = reinterpret_cast<jclass>(env->NewGlobalRef(dexposedClass));
+ if (dexposedClass == NULL) {
+        LOGE("Error while loading Dexposed class '%s':\n", DEXPOSED_CLASS);
+        dvmLogExceptionStackTrace();
+        env->ExceptionClear();
+        return false;
+    }
+
+ LOGI("Found Dexposed class '%s', now initializing\n", DEXPOSED_CLASS);
+    if (register_com_taobao_android_dexposed_DexposedBridge(env) != JNI_OK) {
+        LOGE("Could not register natives for '%s'\n", DEXPOSED_CLASS);
+        return false;
+    }
+
+    return true;
+
 }
+
+static jboolean initNative(JNIEnv* env, jclass clazz) {
+
+    dexposedHandleHookedMethod = (Method*) env->GetStaticMethodID(dexposedClass, "handleHookedMethod",
+        "(Ljava/lang/reflect/Member;ILjava/lang/Object;Ljava/lang/Object;[Ljava/lang/Object;)Ljava/lang/Object;");
+    if (dexposedHandleHookedMethod == NULL) {
+        LOGE("ERROR: could not find method %s.handleHookedMethod(Member, int, Object, Object, Object[])\n", DEXPOSED_CLASS);
+        dvmLogExceptionStackTrace();
+        env->ExceptionClear();
+        keepLoadingDexposed = false;
+        return false;
+    }
+
+    objectArrayClass = dvmFindArrayClass("[Ljava/lang/Object;", NULL);
+    if (objectArrayClass == NULL) {
+        LOGE("Error while loading Object[] class");
+        dvmLogExceptionStackTrace();
+        env->ExceptionClear();
+        keepLoadingDexposed = false;
+        return false;
+    }
+
+    return true;
+}
+
 extern "C" JNIEXPORT jint JNI_OnLoad(JavaVM* vm, void* reserved)
 {
     JNIEnv* env = NULL;
@@ -171,6 +446,8 @@ extern "C" JNIEXPORT jint JNI_OnLoad(JavaVM* vm, void* reserved)
 dexposedInfo();
  keepLoadingDexposed = isRunningDalvik();
  keepLoadingDexposed = dexposedOnVmCreated(env, NULL);
+
+  initNative(env, NULL);
 
     return JNI_VERSION_1_6;
  }
--- dexposedj/jni/dexposed.h
+++ dexposedj/jni/dexposed.h
@@ -21,4 +21,232 @@ const char* dexposedOffsetModesDesc[] = {
     "NO_JIT",
 };
 
+typedef unsigned int u4;
+struct ClassObject;
+
+struct Object {
+    /* ptr to class object */
+    ClassObject*    clazz;
+
+    /*
+     * A word containing either a "thin" lock or a "fat" monitor.  See
+     * the comments in Sync.c for a description of its layout.
+     */
+    u4              lock;
+};
+
+#define CLASS_FIELD_SLOTS   4
+
+struct ClassObject : Object {
+    /* leave space for instance data; we could access fields directly if we
+       freeze the definition of java/lang/Class */
+    u4              instanceData[CLASS_FIELD_SLOTS];
+
+    /* UTF-8 descriptor for the class; from constant pool, or on heap
+       if generated ("[C") */
+    const char*     descriptor;
+    char*           descriptorAlloc;
+};
+
+typedef unsigned short u2;
+
+//typedef void (*DalvikBridgeFunc)(const u4* args, JValue* pResult,
+//    const Method* method, struct Thread* self);
+
+//struct DexProto {
+//    const DexFile* dexFile;     /* file the idx refers to */
+ //   u4 protoIdx;                /* index into proto_ids table of dexFile */
+//};
+struct DexProto {
+    const void* dexFile;     /* file the idx refers to */
+    u4 protoIdx;                /* index into proto_ids table of dexFile */
+};
+
+typedef void (*DalvikBridgeFunc)(const u4* args, void* pResult,
+    const void* method, void* self);
+
+struct Method {
+    /* the class we are a part of */
+    ClassObject*    clazz;
+
+    /* access flags; low 16 bits are defined by spec (could be u2?) */
+    u4              accessFlags;
+
+    /*
+     * For concrete virtual methods, this is the offset of the method
+     * in "vtable".
+     *
+     * For abstract methods in an interface class, this is the offset
+     * of the method in "iftable[n]->methodIndexArray".
+     */
+    u2             methodIndex;
+
+    /*
+     * Method bounds; not needed for an abstract method.
+     *
+     * For a native method, we compute the size of the argument list, and
+     * set "insSize" and "registerSize" equal to it.
+     */
+    u2              registersSize;  /* ins + locals */
+    u2              outsSize;
+    u2              insSize;
+
+    /* method name, e.g. "<init>" or "eatLunch" */
+    const char*     name;
+
+    /*
+     * Method prototype descriptor string (return and argument types).
+     *
+     * TODO: This currently must specify the DexFile as well as the proto_ids
+     * index, because generated Proxy classes don't have a DexFile.  We can
+     * remove the DexFile* and reduce the size of this struct if we generate
+     * a DEX for proxies.
+     */
+    DexProto        prototype;
+
+    /* short-form method descriptor string */
+    const char*     shorty;
+
+    /*
+     * The remaining items are not used for abstract or native methods.
+     * (JNI is currently hijacking "insns" as a function pointer, set
+     * after the first call.  For internal-native this stays null.)
+     */
+
+    /* the actual code */
+    const u2*       insns;          /* instructions, in memory-mapped .dex */
+
+    /* JNI: cached argument and return-type hints */
+    int             jniArgInfo;
+
+    /*
+     * JNI: native method ptr; could be actual function or a JNI bridge.  We
+     * don't currently discriminate between DalvikBridgeFunc and
+     * DalvikNativeFunc; the former takes an argument superset (i.e. two
+     * extra args) which will be ignored.  If necessary we can use
+     * insns==NULL to detect JNI bridge vs. internal native.
+     */
+    DalvikBridgeFunc nativeFunc;
+
+    /*
+     * JNI: true if this static non-synchronized native method (that has no
+     * reference arguments) needs a JNIEnv* and jclass/jobject. Libcore
+     * uses this.
+     */
+    bool fastJni;
+
+    /*
+     * JNI: true if this method has no reference arguments. This lets the JNI
+     * bridge avoid scanning the shorty for direct pointers that need to be
+     * converted to local references.
+     *
+     * TODO: replace this with a list of indexes of the reference arguments.
+     */
+    bool noRef;
+
+    /*
+     * JNI: true if we should log entry and exit. This is the only way
+     * developers can log the local references that are passed into their code.
+     * Used for debugging JNI problems in third-party code.
+     */
+    bool shouldTrace;
+
+    /*
+     * Register map data, if available.  This will point into the DEX file
+     * if the data was computed during pre-verification, or into the
+     * linear alloc area if not.
+     */
+  //  const RegisterMap* registerMap;
+void* registerMap;
+    /* set if method was called during method profiling */
+    bool            inProfile;
+};
+
+struct DexposedHookInfo {
+    struct {
+        Method originalMethod;
+        // copy a few bytes more than defined for Method in AOSP
+        // to accomodate for (rare) extensions by the target ROM
+        int dummyForRomExtensions[4];
+    } originalMethodStruct;
+
+    Object* reflectedMethod;
+    Object* additionalInfo;
+};
+#define SET_METHOD_FLAG(method, flag) \
+    do { (method)->accessFlags |= (flag); } while (0)
+
+enum {
+    ACC_PUBLIC       = 0x00000001,       // class, field, method, ic
+    ACC_PRIVATE      = 0x00000002,       // field, method, ic
+    ACC_PROTECTED    = 0x00000004,       // field, method, ic
+    ACC_STATIC       = 0x00000008,       // field, method, ic
+    ACC_FINAL        = 0x00000010,       // class, field, method, ic
+    ACC_SYNCHRONIZED = 0x00000020,       // method (only allowed on natives)
+    ACC_SUPER        = 0x00000020,       // class (not used in Dalvik)
+    ACC_VOLATILE     = 0x00000040,       // field
+    ACC_BRIDGE       = 0x00000040,       // method (1.5)
+    ACC_TRANSIENT    = 0x00000080,       // field
+    ACC_VARARGS      = 0x00000080,       // method (1.5)
+    ACC_NATIVE       = 0x00000100,       // method
+    ACC_INTERFACE    = 0x00000200,       // class, ic
+    ACC_ABSTRACT     = 0x00000400,       // class, method, ic
+    ACC_STRICT       = 0x00000800,       // method
+    ACC_SYNTHETIC    = 0x00001000,       // field, method, ic
+    ACC_ANNOTATION   = 0x00002000,       // class, ic (1.5)
+    ACC_ENUM         = 0x00004000,       // class, field, ic (1.5)
+    ACC_CONSTRUCTOR  = 0x00010000,       // method (Dalvik only)
+    ACC_DECLARED_SYNCHRONIZED =
+                       0x00020000,       // method (Dalvik only)
+    ACC_CLASS_MASK =
+        (ACC_PUBLIC | ACC_FINAL | ACC_INTERFACE | ACC_ABSTRACT
+                | ACC_SYNTHETIC | ACC_ANNOTATION | ACC_ENUM),
+    ACC_INNER_CLASS_MASK =
+        (ACC_CLASS_MASK | ACC_PRIVATE | ACC_PROTECTED | ACC_STATIC),
+    ACC_FIELD_MASK =
+        (ACC_PUBLIC | ACC_PRIVATE | ACC_PROTECTED | ACC_STATIC | ACC_FINAL
+                | ACC_VOLATILE | ACC_TRANSIENT | ACC_SYNTHETIC | ACC_ENUM),
+    ACC_METHOD_MASK =
+        (ACC_PUBLIC | ACC_PRIVATE | ACC_PROTECTED | ACC_STATIC | ACC_FINAL
+                | ACC_SYNCHRONIZED | ACC_BRIDGE | ACC_VARARGS | ACC_NATIVE
+                | ACC_ABSTRACT | ACC_STRICT | ACC_SYNTHETIC | ACC_CONSTRUCTOR
+                | ACC_DECLARED_SYNCHRONIZED),
+};
+
+typedef uint64_t            u8;
+
+enum {
+    ALLOC_DEFAULT = 0x00,
+    ALLOC_DONT_TRACK = 0x01,  /* don't add to internal tracking list */
+    ALLOC_NON_MOVING = 0x02,
+};
+
+struct ArrayObject : Object {
+    /* number of elements; immutable after init */
+    u4              length;
+
+    /*
+     * Array contents; actual size is (length * sizeof(type)).  This is
+     * declared as u8 so that the compiler inserts any necessary padding
+     * (e.g. for EABI); the actual allocation may be smaller than 8 bytes.
+     */
+    u8              contents[1];
+};
+typedef unsigned char u1;
+typedef   char s1;
+typedef short s2;
+typedef int s4;
+typedef long long s8;
+union JValue {
+    u1      z;
+    s1      b;
+    u2      c;
+    s2      s;
+    s4      i;
+    s8      j;
+    float   f;
+    double  d;
+    Object* l;
+};
+
 #endif //VENUXFIX_DEXPOSED_H
